14a20932e52e0850c92bfa96855f820a7827a100
surajupare <surajkupare@gmail.com>
2019-04-28 17:01:29 +0530

    Initial Commit

---
+++ DatingApp.API/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+//CreateDefaultBuilder - use Kestrel as the web server and 
+//configure it using the application's configuration. it is light wight web server. send request to http server and get responce.
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ DatingApp.API/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using DatingApp.API.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.UseSqlite;
+
+namespace DatingApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // We havily used this method for dependency injection.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //like connection string
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));// it will point to appSetting.json Connection string
+           
+            //use dot net FM for web API.
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to 
+        //configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //for development
+            if (env.IsDevelopment())
+            {
+                 app.UseDeveloperExceptionPage(); // it will show exception details
+            }
+            else
+            {
+                //security enhancement
+                //app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseMvc();
+        }
+    }
+}
