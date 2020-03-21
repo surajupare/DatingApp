aac700e4fa758762043360c66cf2417f7b47ce4d
surajupare <surajkupare@gmail.com>
2020-03-21 15:36:00 +0530

    End of section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -21,7 +21,7 @@ export class NavComponent implements OnInit {
       console.log('logged in successful');
     },
     error => {
-      console.log('logged in failed');
+      console.log(error);
     });
   }
 
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -16,6 +16,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 //using Microsoft.EntityFrameworkCore.UseSqlite;
 
 namespace DatingApp.API
@@ -68,6 +72,19 @@ namespace DatingApp.API
             }
             else
             {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+                //app.UseExceptionHander();
                 //security enhancement
                 //app.UseHsts();
             }
